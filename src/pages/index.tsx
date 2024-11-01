9f3212b90bf5303a42c44083f891fe93dd716913
donguk <donguk@rapportlabs.kr>
2024-11-01 16:25:45 +0900

    add background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,19 @@
+const METRIC = {
+    BG_HEIGHT: 5000
+}
+
 export default function Home() {
-  return (
-    <>
-      <div/>
-    </>
-  );
+    return (
+        <>
+            <div
+                style={{position: 'relative', width: '100%', height: METRIC.BG_HEIGHT}}
+            >
+                <img
+                    src={'/background.png'}
+                    width={'100%'}
+                    height={METRIC.BG_HEIGHT}
+                />
+            </div>
+        </>
+    );
 }
