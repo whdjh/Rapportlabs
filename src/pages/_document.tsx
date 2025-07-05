3b571174f187a30d88488a242c41387ffeba5972
whdjh <whdjh98@naver.com>
2025-07-05 18:59:44 +0900

    feat: 메타태그 추가 및 타이틀 추가 및 색상 대비도 추가하여 성능제외 lighthouse 점수 상승

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,16 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ko">
+      <Head>
+        <title>라포랩스 인턴 과제 전형</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="description" content="퀸잇 유저를 위한 모바일 사과 낙하 게임 과제" />
+        <meta name="theme-color" content="#7B61FF" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </Head>
       <body>
         <Main />
         <NextScript />
